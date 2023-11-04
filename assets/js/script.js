0b09379144ec77bfeb4c3ce38c50c7686e008d70
Donquis <juandonquis07@gmail.com>
2023-11-04 17:33:27 -0400

    firsts steps to main section

--- assets/js/script.js
+++ assets/js/script.js
@@ -143,20 +143,40 @@
 // });
 
 
-const width = window.innerWidth;
+// Set scroll Reveal
+ScrollReveal({
 
-// Si el ancho es menor de 768px
-if (width < 768) {
-  // Obtener todos los elementos con el atributo aria-hidden
-  const elements = document.querySelectorAll('[aria-hidden]');
+	reset:true,
+	distance:'60px',
+	duration:2500,
+	delay:400
+});
 
-  // Iterar sobre los elementos
-  for (const element of elements) {
-    // Obtener el valor actual del atributo aria-hidden
-    const hidden = element.getAttribute('aria-hidden');
+ScrollReveal().reveal('.menu-nav',{delay:0, easing:'ease-in-out',duration:900,origin:'top'});
+// ScrollReveal().reveal('.circle-left',{interval:200, easing:'ease-in-out',duration:900,origin:'left'});
+ScrollReveal().reveal('.box-typed-text',{interval:200, easing:'ease-in-out',duration:700,origin:'right'});
+ScrollReveal().reveal('.introduce-card',{delay:1000, easing:'ease-in-out',duration:700,origin:'right'});
+ScrollReveal().reveal('.social-link',{delay:1200, easing:'ease-in-out',duration:700,origin:'bottom'});
+
+
+
+
+
+
+// Set all aria hidden depends window width
+const windowWidth = window.innerWidth;
+
+if (windowWidth < 768) {
+
+  const elementsWithAriaHidden = document.querySelectorAll('[aria-hidden]');
+
+  for (const element of elementsWithAriaHidden) 
+  {
+
+    const ariaHiddenOfElement = element.getAttribute('aria-hidden');
 
     // Si el valor es false, establecerlo en true
-    if (hidden === 'false') {
+    if (ariaHiddenOfElement === 'false') {
       element.setAttribute('aria-hidden', 'true');
     } else {
       // Si el valor es true, establecerlo en false
